b323e050a9871054af7d824517ce954e74391f72
Justin Lim <justinlim@Justins-MacBook-Air.local>
2026-06-29 12:01:52 +0800

    refactor

--- main.py
+++ main.py
@@ -11,17 +11,17 @@ class LogAnalyzer:
         self.log_path = log_path
         self.conn = duckdb.connect()
 
-    def parse_log(self) -> duckdb.DuckDBPyRelation:
-        return self.conn.sql(f"""
-        SELECT
-            column0 AS ip,
-            regexp_extract(column3, '[0-9].+$') AS date_time,
-            regexp_extract(column5, '^[A-Z]+') AS rest_method,
-            regexp_extract(column5, '/.+ ') AS rest_path,
-            column6 AS return_status,
-            column7 AS bytes
-        FROM read_csv('{self.log_path}', delim=' ')
-    """)
+        self.conn.sql(f"""
+            CREATE OR REPLACE TEMP VIEW parsed_logs AS
+            SELECT
+                column0 AS ip,
+                regexp_extract(column3, '[0-9].+$') AS date_time,
+                regexp_extract(column5, '^[A-Z]+') AS rest_method,
+                regexp_extract(column5, '/.+ ') AS rest_path,
+                column6 AS return_status,
+                column7 AS bytes
+            FROM read_csv('{self.log_path}', delim=' ')
+            """)
 
     def get_top_requests(self) -> pl.DataFrame:
         parsed_query = self.parse_log()
@@ -38,9 +38,8 @@ class JsonLogAnalyzer:
     def __init__(self, log_path):
         self.log_path = log_path
         self.conn = duckdb.connect()
-
-    def parse_log(self) -> duckdb.DuckDBPyRelation:
-        return self.conn.sql(f"""
+        self.conn.sql(f"""
+            CREATE OR REPLACE TEMP VIEW parsed_logs AS
             SELECT
                 remote_ip AS ip,
                 time AS date_time,
@@ -51,31 +50,39 @@ class JsonLogAnalyzer:
             FROM read_json('{self.log_path}')
         """)
 
+    def get_daily_info(self) -> pl.DataFrame:
+        return self.conn.sql("""
+            SELECT date_time, COUNT(*) AS total_requests,
+            COUNT(DISTINCT ip) AS unique_ips,
+            COUNT(DISTINCT rest_path) AS unique_paths,
+            SUM(bytes) AS total_bytes
+            FROM parsed_logs
+            GROUP BY ALL
+            ORDER BY ALL
+            """).pl()
+
     def get_top_requests(self) -> pl.DataFrame:
-        parsed_query = self.parse_log()
         return self.conn.sql("""
             SELECT * EXCLUDE(date_time, bytes), COUNT(*) AS count
-            FROM parsed_query
+            FROM parsed_logs
             GROUP BY ALL
             ORDER BY count DESC
             LIMIT 5
             """).pl()
 
     def get_top_rest_path_status(self) -> pl.DataFrame:
-        parsed_query = self.parse_log()
         return self.conn.sql("""
             SELECT CONCAT(rest_method, ' ', rest_path) AS rest_path_status, return_status, COUNT(*) AS count
-            FROM parsed_query
+            FROM parsed_logs
             GROUP BY ALL
             ORDER BY count DESC
             LIMIT 5
             """).pl()
 
     def get_top_errors_by_ip(self) -> pl.DataFrame:
-        parsed_query = self.parse_log()
         return self.conn.sql("""
             SELECT ip, return_status, COUNT(*) AS count
-            FROM parsed_query
+            FROM parsed_logs
             WHERE return_status >= 400 AND return_status < 600
             GROUP BY ALL
             ORDER BY count DESC
@@ -83,10 +90,9 @@ class JsonLogAnalyzer:
             """).pl()
 
     def get_top_errors_by_rest_path(self) -> pl.DataFrame:
-        parsed_query = self.parse_log()
         return self.conn.sql("""
             SELECT CONCAT(rest_method, ' ', rest_path) AS rest_path_status, return_status, COUNT(*) AS count
-            FROM parsed_query
+            FROM parsed_logs
             WHERE return_status >= 400 AND return_status < 600
             GROUP BY ALL
             ORDER BY count DESC
@@ -141,6 +147,9 @@ def frontend():
                 st.write("Top Errors by Rest Path:")
                 top_errors_by_rest_path = log_analyzer.get_top_errors_by_rest_path()
                 st.dataframe(top_errors_by_rest_path)
+                st.write("Daily Info:")
+                daily_info = log_analyzer.get_daily_info()
+                st.dataframe(daily_info)
 
                 st.subheader("Bar Chart")
                 st.write("Top Errors by IP")
@@ -150,7 +159,7 @@ def frontend():
                     y="count",
                     labels={"ip": "IP Address", "count": "Error Count"},
                 )
-                st.plotly_chart(fig, use_container_width=True)
+                st.plotly_chart(fig, width=True)
             case _:
                 st.write("Unsupported file format")
 
